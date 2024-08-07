e9d54deb1f1f4ffe91c1b3a4f57e822901b21886
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-08-07 13:59:44 -0300

    Add more functions

--- commands.py
+++ commands.py
@@ -1,4 +1,26 @@
+import socket
+import json
+
 class Command:
-    def __init__(self, pid, description) -> None:
+    def __init__(self, pid) -> None:
+        '''Inicializa a classe PID Command - pid = 0100 (comando PID)'''
         self.pid = pid
-        self.desc = description
+    
+    def connection(self, host, port):
+        '''Estabelece a conexão com o OBD Scanner'''
+        data = open('./pid.json')
+        read_json = json.load(data)
+        self.host = host
+        self.port = port
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ser:
+            ser.connect((self.host, self.port))
+            try:
+                ser.sendall(bytes.fromhex(self.pid))
+                self.response = ser.recv(1024)
+                print(f"Resposta: {self.response}")
+                print(f"\nDescrição do comando: {read_json.get(self.pid)}")
+                socket.close()
+            except Exception as e:
+                print(f"Erro: {e}")
+                socket.close()
+        
--- obd_connect.py
+++ obd_connect.py
@@ -1,22 +1,10 @@
 from commands import Command
-import socket
-     
-     
+
 pid = input("Digite o número do PID: ")
-desc = input("\nDigite a descrição do PID: ")
         
-command = Command(pid, desc)
+command = Command(pid)
 
 host= '192.168.0.10'
 port = 35000
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ser:
-    ser.connect((host, port))
-    try:
-        ser.sendall(bytes.fromhex(command.pid))
-        response = ser.recv(1024)
-        print(f"Resposta: {response}")
-        print(f"\nDescrição do comando: {command.desc}")
-    except Exception as e:
-        print(f"Erro: {e}")
-        socket.close()
+command.connection(host, port)
